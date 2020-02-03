3df5ae3e3d6c13ba73492a2ebefa7f7d146943a2
gokuldas <gokuldas@192.168.100.53>
2020-02-04 01:50:48 +1100

    New project

---
+++ newproject/src/test/java/goksselenium/credentialTest.java
@@ -0,0 +1,15 @@
+package goksselenium;
+
+import org.testng.annotations.Test;
+
+
+public class credentialTest {
+	
+	@Test
+	public void loginnew()
+	{
+		
+		System.out.println("google new");
+	}
+
+}
---
+++ newproject/src/test/java/goksselenium/loginTest.java
@@ -0,0 +1,16 @@
+package goksselenium;
+
+import org.testng.annotations.Test;
+
+
+public class loginTest {
+	
+	
+	@Test
+	public void login()
+	{
+		
+		System.out.println("google");
+	}
+
+}
